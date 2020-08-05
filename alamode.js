f681de962f6e0afb07ca53fb48951e405e6cf673
Philip Reasa <philipreasa@gmail.com>
2020-08-05 08:52:10 -0500

    a

--- alamode.js
+++ alamode.js
@@ -796,7 +796,7 @@ var alamode = {
     tr.append("td")
       .attr("class", "data value")
       .text(function (d) {
-        return d;
+        return d.verified;
       });
 
     // Create a column at the beginning of the table for the chart
